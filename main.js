7122967d915d902911c05de77fda1fe3562c3927
Fredrik Söderström <tirithen@gmail.com>
2013-12-08 00:18:04 +0100

    Rewrite of TyGoss/Code-Font extension
    This version has much cleaner code and a diffent set of all monotype fonts

--- main.js
+++ main.js
@@ -1,93 +1,104 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
+/* global brackets, define, $ */
 
-define(function (require, exports, module) {
-    "use strict";
-	
-	var CommandManager = brackets.getModule("command/CommandManager"),
-		Menus = brackets.getModule("command/Menus"),
-		PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
-		ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+define(function () {
+    'use strict';
 
-	ExtensionUtils.loadStyleSheet(module, "fonts.css");
-	
-	var preferences = PreferencesManager.getPreferenceStorage("extensions.code-font"), 
-		menu = Menus.addMenu("Font", "code-font", Menus.AFTER, Menus.AppMenuBar.VIEW_MENU);
-	
-	// the array of fonts
-	var fonts = [["code-font.default","Default"],["code-font.open-sans","Open Sans"],["code-font.roboto","Roboto"],["code-font.droid-sans","Droid Sans"],["code-font.lato","Lato"],["code-font.droid-serif","Droid Serif"],["code-font.ubuntu","Ubuntu"],["code-font.raleway","Raleway"],["code-font.oxygen","Oxygen"],["code-font.shadows-into-light","Shadows Into Light"]];
+    var fonts = [ // Add or remove fonts here from Google Fonts
+        'Default', // This item is needed to reset to the standard font
+        'Cutive Mono',
+        'Droid Sans Mono',
+        'Inconsolata',
+        'Nova Mono',
+        'Oxygen Mono',
+        'PT Mono',
+        'Share Tech Mono',
+        'Ubuntu Mono'
+    ];
+    
+	var CommandManager = brackets.getModule('command/CommandManager'),
+        Menus = brackets.getModule('command/Menus'),
+        PreferencesManager = brackets.getModule('preferences/PreferencesManager'),
+        menuId = 'font-menu',
+        preferences = PreferencesManager.getPreferenceStorage('extensions.brackets-monospace-fonts'),
+		menu = Menus.getMenu(menuId) || Menus.addMenu('Font', menuId, Menus.AFTER, Menus.AppMenuBar.VIEW_MENU),
+        editorHolderElement = $('#editor-holder'),
+        bodyElement = $('body');
+            
+    function Font(name) {
+        this.name = name || '';
+        this.className = 'code-font-' + this.name.replace(/\s/g, '-').replace(/[^\w\d]/g, '').toLowerCase();
+        
+        if(name !== 'Default') {
+            this.nameForURI = this.name.replace(/\s/g, '+').replace(/[^\w\d\+]/g, '');
+            this.linkElement = $('<link class="' + this.className + 
+                                            '" href="http://fonts.googleapis.com/css?family=' +
+                                            this.nameForURI + '" rel="stylesheet" type="text/css">');
+            this.css = '.' + this.className + ' .CodeMirror {' +
+                            'font-family: \'' + this.name + '\', sans-serif !important;}';
+            this.hasLinkTagAppendedToBody = false;
+        }
+    }
+            
+    function getFontByName(name) {
+        var font, i = 0, length = fonts.length;
+        
+        for(i = 0; i < length; i++) {
+            if(fonts[i].name === name) {
+                font = fonts[i];
+                break;
+            }
+        }
+        
+        return font;
+    }
+    
+    function changeFont(name) {
+        var font = getFontByName(name);
+        
+        if(font) {
+            editorHolderElement.attr('class', '');
+            preferences.setValue('selectedFont', font.name);
+            
+            if(font.linkElement && font.className) {
+                if(!font.hasLinkTagAppendedToBody) {
+                    bodyElement.append(font.linkElement);
+                    font.hasLinkTagAppendedToBody = true;
+                }
+                
+                editorHolderElement.attr('class', font.className);
+            }
+        }
+    }
 
-	// function that changes the font, the font name to change to is passed to the function
-	function changeFont(font){
-		
-		$("#editor-holder").attr("class","");
-				
-		if(font === fonts[0][1]){
-			//default don't do anything	
-		}
-		else if(font === fonts[1][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-open-sans");
-		}else if(font === fonts[2][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-roboto");
-		}
-		else if(font === fonts[3][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-droid-sans");
-		}
-		else if(font === fonts[4][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-lato");
-		}
-		else if(font === fonts[5][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-droid-serif");
-		}
-		else if(font === fonts[6][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-ubuntu");
-		}
-		else if(font === fonts[7][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-raleway");
-		}
-		else if(font === fonts[8][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-oxygen");
-		}
-		else if(font === fonts[9][1]){
-			$('body').append("<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>");
-			$("#editor-holder").addClass("code-font-shadows-into-light");
-		}
-	}	
-	
-	//get the current saved font
-	var selectedFont = preferences.getValue("selectedFont");
-	
-	// If there is no font selected, use default font
-	if (selectedFont === undefined) {
-		preferences.setValue("selectedFont", "Default");
-		selectedFont = preferences.getValue("selectedFont");
-		return;
+	function createFontMenu() {
+        fonts.forEach(function (font) {
+            CommandManager.register(font.name, font.className, function () {
+                changeFont(font.name);
+            });
+            menu.addMenuItem(font.className);	
+        });
+    }
+
+    function createFontStyleElement() {
+        var css = fonts.map(function (font) {
+            return font.css;
+        }).join(' ');
+
+        $('<style>' + css + '</style>').appendTo(bodyElement);
+    }
+
+	// Set default value if no font is specified
+	if (!preferences.getValue('selectedFont')) {
+		changeFont('Default');
 	}
-	
-	//function to create each font menu item and it's command
-	function createFontMenu(i){
-		CommandManager.register(fonts[i][1], fonts[i][0] , function () {
-			preferences.setValue("selectedFont", fonts[i][1]);
-			selectedFont = preferences.getValue("selectedFont");
-			changeFont(selectedFont);
-		});
-		menu.addMenuItem(fonts[i][0]);	
-	}
-	
-	// loop through the array of fonts to create the menu
-	for(var i=0; i < fonts.length; i++){
-		createFontMenu(i);
-	}
-	
-	//set the font on page load
-	changeFont(selectedFont);
 
+    // Create font instances
+    fonts = fonts.map(function (fontName) { 
+        return new Font(fontName); 
+    });
+
+    // Create menu items and set the font
+    createFontStyleElement();
+    createFontMenu();
+    changeFont(preferences.getValue('selectedFont'));
 });
